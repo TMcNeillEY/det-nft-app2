627f1b3c172109a0f950ce8c59d090455d0eea1b
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-06 18:32:00 -0500

    updated whitelist address

--- hardhat-v2/constants/index.js
+++ hardhat-v2/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x3Ec92D0E5F1f157fBc93d9cA05Eb554BcCaF4C5e";
+const WHITELIST_CONTRACT_ADDRESS = "0xBB03cf83424E1d98853E8e96070C0D72dBE553B7";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "det-nft-app2.vercel.app/api/";
 
@@ -7,3 +7,5 @@ module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
 // first contract address: 0x0C0Fcc0215B81630425902C11654e83c320E3B87
 // DET Devs 2 address: 0x92885a58855e5FBae290f0463a99401d155a9c32
+//DET Devs Contract Address 3: 0xA29a62881fE539D3a6559410A6533caF9F94F52e
+//Whitelist Contract Address: 0xBB03cf83424E1d98853E8e96070C0D72dBE553B7
