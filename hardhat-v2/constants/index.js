d0edf97f7193230bcff77d4d4a65192c372cd5fa
TMcNeillEY <Tiquila.McNeill@ey.com>
2023-01-07 02:44:00 -0500

    working on the metadata url

--- hardhat-v2/constants/index.js
+++ hardhat-v2/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xBB03cf83424E1d98853E8e96070C0D72dBE553B7";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "det-nft-app2.vercel.app/api/";
+const METADATA_URL = `det-nft-app2.vercel.app/api/${tokenId}`;
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
